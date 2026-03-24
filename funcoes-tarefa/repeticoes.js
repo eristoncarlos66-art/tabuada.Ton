12b816208a927d0fa733a6cee5870499fd225ab0
eristoncarlos66-art <eristoncarlos66@gmail.com>
2026-03-24 11:12:20 -0300

    Add files via upload

---
+++ funcoes-tarefa/repeticoes.js
@@ -0,0 +1,18 @@
+//let numero = 0
+/*
+while(numero < 10){
+    console.log("Estou repetindo")
+    numero = numero + 1
+}
+
+console.log("sai do laço") */
+
+
+let tabuada = prompt("Escolha sua Tabuada")
+let contador = 0
+
+while (contador <= 10){
+    console.log(tabuada + " x " + contador + " = " + 
+    tabuada * contador)
+    contador = contador + 1
+}
